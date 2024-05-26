e6716f88272f2662714384a90b57cdf5f89fcb23
anandgru <anandverma6225@gmail.com>
2024-05-26 14:28:55 +0530

    fuly running

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const h=document.getElementById("main-heading")
+h.textContent='Fruit World';
+h.style.color='orange';
 const header=document.getElementById("header");
+
 header.style.backgroundColor='green';
+header.style.borderBottom='3px solid orange';
 const heading=document.getElementById("basket-heading")
-header.style.color='orange';
+
+
+
+
 heading.style.color='green';
 const thanks=document.getElementById("thanks")
-thanks.innerHTML='<p>Please visit us again.</p>';
+thanks.innerHTML='<p>Please visit us again</p>';

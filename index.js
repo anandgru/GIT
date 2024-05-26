85ed8c5ee3b27fdf597b949a04109472f5a4a3b5
anandgru <anandverma6225@gmail.com>
2024-05-26 13:09:45 +0530

    Changed attribute by get element id

---
+++ index.js
@@ -0,0 +1,7 @@
+const header=document.getElementById("header");
+header.style.backgroundColor='green';
+const heading=document.getElementById("basket-heading")
+header.style.color='orange';
+heading.style.color='green';
+const thanks=document.getElementById("thanks")
+thanks.innerHTML='<p>Please visit us again.</p>';
